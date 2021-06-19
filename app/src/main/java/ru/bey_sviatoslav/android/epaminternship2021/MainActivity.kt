4b81c3ff3acf0409a6773a82792400784efd95ca
Daniel <Vasiliy23898@gmail.com>
2021-06-19 20:53:33 +0300

    pinch

--- app/src/main/java/ru/bey_sviatoslav/android/epaminternship2021/MainActivity.kt
+++ app/src/main/java/ru/bey_sviatoslav/android/epaminternship2021/MainActivity.kt
@@ -2,10 +2,26 @@ package ru.bey_sviatoslav.android.epaminternship2021
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ViewTreeObserver
+import ru.bey_sviatoslav.android.epaminternship2021.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+
+        val observer: ViewTreeObserver = binding.drawableImage.viewTreeObserver
+        observer.addOnGlobalLayoutListener {
+            binding.scaleFactorValue.text = binding.drawableImage.mScaleFactor.toString()
+        }
+
+        supportActionBar?.hide()
+
+        setContentView(view)
     }
 }
