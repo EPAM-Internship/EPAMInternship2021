dd09e13f00ce703e5e47e92388bf73796a049df2
Daniel <Vasiliy23898@gmail.com>
2021-06-27 23:27:39 +0300

    zoom HD texture of camera in 60 FPS

--- app/src/main/java/ru/bey_sviatoslav/android/epaminternship2021/layer/LayoutManager.kt
+++ app/src/main/java/ru/bey_sviatoslav/android/epaminternship2021/layer/LayoutManager.kt
@@ -10,9 +10,10 @@ class LayoutManager(view: CustomView) {
     private val boardTopBitmap = BitmapFactory.decodeResource(view.resources, R.drawable.board_top)
     private val boardBottomBitmap = BitmapFactory.decodeResource(view.resources, R.drawable.board_bottom)
     private val batteryBitmap = BitmapFactory.decodeResource(view.resources, R.drawable.battery)
+    private val cameraBitmap = BitmapFactory.decodeResource(view.resources, R.drawable.camera)
 
     val backBitmap: Bitmap = BitmapFactory.decodeResource(view.resources, R.drawable.back)
     val firstLayerAction = LayerAction(1.0f, 1.4999f, backBitmap, listOf(insidesBitmap))
-    val secondLayerAction = LayerAction(1.5f, 1.9999f, insidesBitmap, listOf(boardBottomBitmap, batteryBitmap, boardTopBitmap))
-    val thirdLayer = Layer(2.0f, 5.0f, listOf(insidesBitmap, boardBottomBitmap, batteryBitmap, boardTopBitmap))
+    val secondLayerAction = LayerAction(1.5f, 1.9999f, insidesBitmap, listOf(boardBottomBitmap, batteryBitmap, boardTopBitmap, cameraBitmap))
+    val thirdLayer = Layer(2.0f, 5.0f, listOf(insidesBitmap, boardBottomBitmap, batteryBitmap, boardTopBitmap, cameraBitmap))
 }
